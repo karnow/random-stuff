46c0587ded0f1558ef4ff2dcbdb821987b64867f
carl <karol-nowakowski@o2.pl>
2020-12-18 10:22:27 +0100

    Create dynamic rootValue

--- index.js
+++ index.js
@@ -4,17 +4,24 @@ const PORT = process.env.PORT || 4000
 const typeDefs = gql`
 type Query {
     greeting: String,
-    intereStringUrls: [String]
+    intereStringUrls: [String],
+    randomDiceThrow: Int
 }
 
 `;
+function rootValue() {
+
+const getRandomDiceThrow =(sides)=> Math.ceil(Math.random()* sides);
 
 const data = {
-    greeting: "Hello World",
-    intereStringUrls: ["www.onet.pl", "www.wp.pl"]
+    greeting: "Hello orld",
+    intereStringUrls: ["www.onet.pl", "www.wp.pl"],
+    randomDiceThrow: getRandomDiceThrow(6)
+}
+return data;
 }
 
-const server = new ApolloServer({typeDefs, rootValue:data, playground:true, introspection:true});
+const server = new ApolloServer({typeDefs, rootValue, playground:true, introspection:true});
 
 server.listen({port: PORT}).then((result) => console.log(result.url, result.port));
 
