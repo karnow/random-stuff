64b339c75437936c6e0a99dc02269503b02fb814
carl <karol-nowakowski@o2.pl>
2020-12-18 13:08:35 +0100

    Create Folat and Boolean data

--- index.js
+++ index.js
@@ -5,18 +5,36 @@ const typeDefs = gql`
 type Query {
     greeting: String,
     intereStringUrls: [String],
-    randomDiceThrow: Int
+    randomDiceThrow: Int,
+    pi: Float,
+    isTodayFriday: Boolean,
+    randomCoinTossesUntilTrue: [Boolean]
 }
 
 `;
 function rootValue() {
-
+    
+const today =new Date;
 const getRandomDiceThrow =(sides)=> Math.ceil(Math.random()* sides);
+const randomCoinToss =() => Math.random()>0.5;
+const getRandomCoinTossesUntilTrue =()=> {
+    const result =[];
+    do {
+        result.push(randomCoinToss());
+    } while (!result[result.length -1 ]);
+    return result;
+    }
+
+
 
 const data = {
     greeting: "Hello orld",
-    intereStringUrls: ["www.onet.pl", "www.wp.pl"],
-    randomDiceThrow: getRandomDiceThrow(6)
+    intereStringUrls: ["www.onet.pl", "www.wp.pl", 8],
+    randomDiceThrow: getRandomDiceThrow(6),
+    pi: Math.PI,
+    isTodayFriday: today.getDay() === 5,
+    randomCoinTossesUntilTrue:getRandomCoinTossesUntilTrue()
+
 }
 return data;
 }
